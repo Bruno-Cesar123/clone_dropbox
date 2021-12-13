05c2c9e10c61fe1aeb30f6c4fce580e727a736df
Bruno Cesar <brunocesar12386@gmail.com>
2021-12-13 20:39:09 -0300

    style(index): create styles variants

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
+import Section from './components/Section';
+import GlobalStyles from './styles/GlobalStyles';
+
+import data from './data';
 
 function App() {
   return (
-    <h1>Hello world</h1>
+    <>
+      <Section
+        variant='blue'
+        title={data[0].title}
+        description={data[0].description}
+      />
+      <Section
+        variant='beige'
+        title={data[1].title}
+        description={data[1].description}
+      />
+      <Section
+        variant='white'
+        title={data[2].title}
+        description={data[2].description}
+      />
+      <Section
+        variant='black'
+        title={data[3].title}
+        description={data[3].description}
+      />
+      <GlobalStyles />
+    </>
   );
 }
 
